4c0151adacadbe1e6883090147143a6c7a4419bb
Дарий Крыжановский <122232769+DariiKryzhanovskyi@users.noreply.github.com>
2023-03-15 16:17:29 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7DaysStudyPrograming_1_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameOfGuitar = "Yamaha";
+            int attemps = 3;
+            float gradeOfYear = 10.0f;
+            byte maxMoney = 112;
+            sbyte minMoney = 30;
+            short timeOfAttemps = 3;
+            long kilometers = 1000;
+            ulong meters = 50;
+            double phoneLength = 10;
+            ushort guitarStrings = 6;
+
+            
+
+        }
+    }
+}
